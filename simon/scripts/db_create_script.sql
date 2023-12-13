c9f5870132bfbc09ba03fb0673132828a474a754
Simon Janlöv <simon.janlov@gmail.com>
2023-12-13 14:47:52 +0100

    worked on the sql create script. not finished

--- simon/scripts/db_create_script.sql
+++ simon/scripts/db_create_script.sql
@@ -1,7 +1,8 @@
 -- Drop tables
-DROP TABLE IF EXISTS movies_songs;
-DROP TABLE IF EXISTS songs;
-DROP TABLE IF EXISTS songs_temp;
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS movies_recordings;
+DROP TABLE IF EXISTS recordings;
+DROP TABLE IF EXISTS recordings_temp;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS movies_temp;
 DROP TABLE IF EXISTS original_data;
@@ -9,40 +10,52 @@ DROP TABLE IF EXISTS original_data;
 -- Create table for the original data
 CREATE TABLE IF NOT EXISTS original_data(
 	id SERIAL PRIMARY KEY,
-	movie_title VARCHAR(255) NOT NULL,
+	movie_name VARCHAR(255) NOT NULL,
 	year INT,
-	song_title VARCHAR(255),
+	song_name VARCHAR(255),
 	performed_by VARCHAR(500)
 );
 
 -- Import from csv file
-COPY original_data(movie_title, year, song_title, performed_by) 
-FROM 'C:\projekt_2\soundtrack_proj\data\filtered_movie_songs_no_idx.csv' 
+COPY original_data(movie_name, year, song_name, performed_by) 
+FROM 'C:\projekt_2\soundtrack_proj\movie_soundtrack\simon\data\filtered_movie_songs_no_idx.csv' 
 DELIMITER ',' 
 CSV HEADER;
 
 -- Create table with unique moives
-SELECT DISTINCT movie_title
+SELECT DISTINCT movie_name
 INTO movies_temp
 FROM original_data;
 
 -- Create a movie_id based on the movies (into a new table) 
-SELECT ROW_NUMBER() OVER (ORDER BY movie_title) AS id, movie_title
+SELECT ROW_NUMBER() OVER (ORDER BY movie_name) AS movie_id, movie_name
 INTO movies
 FROM movies_temp;
 
--- Add PK to movies
-ALTER TABLE movies ADD PRIMARY KEY (id);
+-- Add PK and release_year column to movies
+ALTER TABLE movies 
+ADD COLUMN release_year INT,
+ADD PRIMARY KEY (movie_id);
+
+-- Insert corresponding year to movies
+UPDATE movies
+SET release_year = original_data.year
+FROM original_data
+WHERE movies.movie_name = original_data.movie_name;
 
 -- Drop table movies_temp
 DROP TABLE movies_temp;
 
---create the rest of the tables and populate the junction tables
--- Create table with unique songs
-SELECT DISTINCT song_title
-INTO songs_temp
+
+-- Create table with unique recordings
+SELECT DISTINCT song_name, performed_by AS artist_name
+INTO recordings_temp
 FROM original_data;
 
+SELECT * FROM recordings_temp;
+
+
+-- KLAR HIT
 -- Create a song_id based on the songs (into a new table) 
 SELECT ROW_NUMBER() OVER (ORDER BY song_title) AS id, song_title
 INTO songs
